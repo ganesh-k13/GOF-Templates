609ed7f2d9346f0c2f287464d27db262f46fc0d1
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-17 21:58:58 +0530

    Fixed Flask structure

--- flaskr/flaskr/views.py
+++ flaskr/flaskr/views.py
@@ -1,4 +1,4 @@
-from hawkeye import app
+from flaskr import app
 
 from flask import Flask,render_template,redirect,url_for,flash, redirect, request, session, abort, jsonify, Response
 from werkzeug import secure_filename
@@ -10,8 +10,4 @@ app.secret_key = 'secretkeyhereplease'
 
 @app.route("/")
 def home():
-    try:
-        res = session["{0}LoggedIn".format(session["accType"])]
-        return render_template("Home/home.html",title="User",userLoggedIn=True)
-    except Exception as e:
-        return render_template("Home/home.html",title="User",userLoggedIn=False)
+	return "Moshi Moshi"
