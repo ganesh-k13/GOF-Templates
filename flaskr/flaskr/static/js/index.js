e29ba093c51a2e6f49093dfc181e4b450238b60b
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-18 12:18:39 +0530

    Added a page each for all patterns

--- flaskr/flaskr/static/js/index.js
+++ flaskr/flaskr/static/js/index.js
@@ -1,49 +1,77 @@
 $(document).ready(function() {
+    
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
+    $("#patternSearchBox").on('focus', function(event) {
+        $(this).css('border-bottom', '5px solid #164719');
+    });
+    $("#patternSearchBox").on('blur', function(event) {
+        $(this).css('border-bottom', '1px solid black');
+    });
 
-	$("#patternSearchBox").on('focus', function(event) {
-		$(this).css('border-bottom', '5px solid #164719');
-	});
-	$("#patternSearchBox").on('blur', function(event) {
-		$(this).css('border-bottom', '1px solid black');
-	});
+    var patternArr = [
+        "state",
+        "adapter",
+        "decorator",
+        "iterator",
+        "mediator",
+        "singleton",
+        "strategy"
+    ];
+    var trie = SuffixTree.fromArray(patternArr);
+    var allPatternEles = $(".patternDivCol");
+    var allPatternEleNames = [];
+    
+    // var opJson = {
+    //     'pattern': 'state',
+    //     'states':[ {
+    //         'name': 'S1'
+    //     }
+    //     ,
+    //     {
+    //         'name': 'S2'
+    //     }
+    //     ,
+    //     {
+    //         'name': 'S3'
+    //     }
+    //     ],
+    //     'functions':[ {
+    //         'name': 'f1', 'param_types': ['int', 'float'], 'param_names': ['i1', 'f1'], 'return': 'void'
+    //     }
+    //     ,
+    //     {
+    //         'name': 'f2', 'param_types': ['int', 'double'], 'param_names': ['i2', 'd2'], 'return': 'double'
+    //     }
+    //     ]
+    // }
 
-	var patternArr = [
-		"state",
-		"adapter",
-		"decorator",
-		"iterator",
-		"mediator",
-		"singleton",
-		"strategy"
-	];
-	var trie = SuffixTree.fromArray(patternArr);
-	var allPatternEles = $(".patternDivCol");
-	var allPatternEleNames = [];
-
-	$(".patternDivCol").each(function(index, el) {
-		allPatternEleNames.push($(this).attr("name"));
-	});	
+    $(".patternDivCol").each(function(index, el) {
+        allPatternEleNames.push($(this).attr("name"));
+    }); 
 
 
-	console.log(allPatternEleNames);
+    console.log(allPatternEleNames);
 
-	console.log(allPatternEles);
-	$("#patternSearchBox").on('input', function(event) {
-		$(".patternDivCol").each(function(index, el) {
-			$(this).css('display', 'none');;
-		});			
-		var res = trie.find($(this).val());
-		if (res && res.length){ //if res is not null and is not empty
-			var intersection = allPatternEleNames.filter(value => -1 !== res.indexOf(value));
+    console.log(allPatternEles);
+    $("#patternSearchBox").on('input', function(event) {
+        $(".patternDivCol").each(function(index, el) {
+            $(this).css('display', 'none');;
+        });         
+        var res = trie.find($(this).val());
+        if (res && res.length){ //if res is not null and is not empty
+            var intersection = allPatternEleNames.filter(value => -1 !== res.indexOf(value));
 
-			$.each(allPatternEleNames, function(outIndex, outVal) {
-				$.each(intersection, function(inIndex, inVal) {
-					if(outVal == inVal)
-					{
-						$("[name='"+inVal+"']").css('display', 'block');
-					}
-				});
-			});
-		}
-	});
+            $.each(allPatternEleNames, function(outIndex, outVal) {
+                $.each(intersection, function(inIndex, inVal) {
+                    if(outVal == inVal)
+                    {
+                        $("[name='"+inVal+"']").css('display', 'block');
+                    }
+                });
+            });
+        }
+    });
 });
--- flaskr/flaskr/views.py
+++ flaskr/flaskr/views.py
@@ -11,3 +11,32 @@ app.secret_key = 'secretkeyhereplease'
 @app.route("/")
 def home():
 	return render_template("index.html")
+
+
+@app.route("/state")
+def state():
+	return render_template("patterns/state.html")
+
+@app.route("/strategy")
+def strategy():
+	return render_template("patterns/strategy.html")
+
+@app.route("/singleton")
+def singleton():
+	return render_template("patterns/singleton.html")
+
+@app.route("/decorator")
+def decorator():
+	return render_template("patterns/decorator.html")
+
+@app.route("/adapter")
+def adapter():
+	return render_template("patterns/adapter.html")
+
+@app.route("/mediator")
+def mediator():
+	return render_template("patterns/mediator.html")
+
+@app.route("/iterator")
+def iterator():
+	return render_template("patterns/iterator.html")
