2b3bc9c04edae6099f4071f4cb1660e53339138e
hardiksurana <hardiksurana01@gmail.com>
2018-11-23 20:58:24 +0530

    integrated front end and back end for state pattern

--- GOF_templates/render.py
+++ GOF_templates/render.py
@@ -1,4 +1,4 @@
-import utils
+# import utils
 from jinja2 import Template
 import os, sys
 
@@ -18,58 +18,57 @@ class State(Pattern):
 	
 	def render(self):
 		# Create output
+		dirname = os.path.dirname(os.path.abspath(__file__))
 		try: 
-			os.makedirs('templates/output')
-			os.makedirs('templates/output/state')
-			os.makedirs('templates/output/state/include')
-			os.makedirs('templates/output/state/obj')
-			os.makedirs('templates/output/state/src')
+			os.makedirs(dirname + '/templates/output')
+			os.makedirs(dirname + '/templates/output/state')
+			os.makedirs(dirname + '/templates/output/state/include')
+			os.makedirs(dirname + '/templates/output/state/obj')
+			os.makedirs(dirname + '/templates/output/state/src')
+
+			print("\n\n\nNew Files Created!\n\n\n")
 		except(FileExistsError):
+			print("\n\n\nNew Files NOT Created! Delete existing files first! \n\n\n")
 			pass
 		
 		# State.h
-		with open('templates/state/include/state.h', 'r') as f:
+		with open(dirname + '/templates/state/include/state.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open('templates/output/state/include/state.h', 'w') as f:
+		with open(dirname + '/templates/output/state/include/state.h', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], states=self.meta['states']))
 		
 		# State.cpp
-		with open('templates/state/src/state.cpp', 'r') as f:
+		with open(dirname + '/templates/state/src/state.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open('templates/output/state/src/state.cpp', 'w') as f:
+		with open(dirname + '/templates/output/state/src/state.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], states=self.meta['states']))
 		
 		# Context.h
-		with open('templates/state/include/context.h', 'r') as f:
+		with open(dirname + '/templates/state/include/context.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open('templates/output/state/include/context.h', 'w') as f:
+		with open(dirname + '/templates/output/state/include/context.h', 'w') as f:
 			f.write(template.render(functions=self.meta['functions']))
 		
 		# Context.cpp
-		with open('templates/state/src/context.cpp', 'r') as f:
+		with open(dirname + '/templates/state/src/context.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open('templates/output/state/src/context.cpp', 'w') as f:
+		with open(dirname + '/templates/output/state/src/context.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions']))
 		
 		# makefile
-		with open('templates/state/makefile', 'r') as f:
+		with open(dirname + '/templates/state/makefile', 'r') as f:
 			template = Template(f.read())
 		
-		with open('templates/output/state/makefile', 'w') as f:
+		with open(dirname + '/templates/output/state/makefile', 'w') as f:
 			f.write(template.render())
 		
 		#test.cpp
-		with open('templates/state/test.cpp', 'r') as f:
+		with open(dirname + '/templates/state/test.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open('templates/output/state/test.cpp', 'w') as f:
+		with open(dirname + '/templates/output/state/test.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], states=self.meta['states']))
-		
-		
-if __name__ == '__main__':
-	s = State(utils.get_input())
-	s.render()
---
+++ README.md
@@ -0,0 +1,14 @@
+Execution Instructions
+```bash
+virtualenv -p python3 venv
+source venv/bin/activate
+cd flaskr/flaskr
+export FLASK_APP=views.py
+export FLASK_DEBUG=1
+flask run
+```
+
+Authors:
+1. Ganesh K
+2. Hardik Mahipal Surana
+3. Rahul Bharadwaj
