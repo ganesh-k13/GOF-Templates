54be3866314a347ce44b1167aac9ae2f13aa99d5
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-11-26 19:53:29 +0530

    Cleaned up debug prints

--- server/flaskr/__init__.py
+++ server/flaskr/__init__.py
@@ -1,20 +1,8 @@
 from flask import Flask
 import sys
-print("----------------------------")
-for p in sys.path:
-	print(p)
-print("----------------------------")
-
-# from ..config import Config
 import server.config as config
 
-print(config)
 app = Flask(__name__)
-# app.config.from_object(Config.DevelopmentConfig)
 app.config.from_object(config.DevelopmentConfig)
-# print("app.config['DEBUG']:",app.config['DEBUG'])
-
-for key,val in app.config.items():
-	print("app.config['{0}']=".format(key),app.config[key])
 
 from server.flaskr import views 
