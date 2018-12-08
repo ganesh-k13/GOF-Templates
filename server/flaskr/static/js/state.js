267ad3223447b6758a56a3209a3d096096d675d9
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-12-08 14:49:52 +0530

    Added JS functionality to add and delete everything

--- server/flaskr/static/js/state.js
+++ server/flaskr/static/js/state.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    var repeatableGroup = $(".repeatableGroup");
+    var repeatableStateListGroup = $(".repeatableStateListGroup");
 
     $("#stateListColDiv").on("click", ".addStateBtn", function(event) {
         console.log("add clicked",$(this));
         $("#stateListColDiv").append("\
-            <div class='input-group repeatableGroup'>\
+            <div class='input-group repeatableStateListGroup'>\
                 <input type='text' class='form-control' placeholder='State Name' aria-label='State Name' aria-describedby='basic-addon2'>\
                 <div class='input-group-append'>\
                     <button class='btn btn-outline-secondary addStateBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another state'>\
@@ -20,45 +20,74 @@ $(document).ready(function() {
 
     $("#stateListColDiv").on("click", ".delStateBtn", function(event) {
         console.log("del clicked",$(this));
-        var eleToBeDeleted = $(this).parents(".repeatableGroup");
+        var eleToBeDeleted = $(this).parents(".repeatableStateListGroup");
         console.log(eleToBeDeleted);
         eleToBeDeleted.remove();
     });
 
-    $("#funcDeclWrapperRow").on('click', '.addParameterTypeBtn', function(event) {
+    $("#funcDeclWrapperRow").on('click', '.addParameterBtn', function(event) {
         console.log("addParameterTypeBtn");
-        $("#paramTypeDiv").append("\
-            <div class='input-group'>\
+        var eleToBeAddedTo = $(this).parents(".paramDiv");
+        eleToBeAddedTo.append("\
+            <div class='input-group repeatableParamListGroup'>\
                 <input type='text' class='form-control' placeholder='Parameter Type' aria-label='Parameter Type' aria-describedby='basic-addon2'>\
-                <div class='input-group-append'>\
-                    <button class='btn btn-outline-secondary addParameterTypeBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another parameter type'>+</button>\
-                    <button class='btn btn-outline-secondary delParameterTypeBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this parameter type'>-</button>\
-                </div>\
-            </div>\
-        ");
-    });
-
-    $("#funcDeclWrapperRow").on('click', '.delParameterTypeBtn', function(event) {
-        console.log("delParameterTypeBtn");
-        $("")
-    });
-    
-    $("#funcDeclWrapperRow").on('click', '.addParameterNameBtn', function(event) {
-        console.log("addParameterNameBtn");
-        $("#paramNameDiv").append("\
-            <div class='input-group'>\
                 <input type='text' class='form-control' placeholder='Parameter Name' aria-label='Parameter Name' aria-describedby='basic-addon2'>\
                 <div class='input-group-append'>\
-                    <button class='btn btn-outline-secondary addParameterNameBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another parameter name'>+</button>\
-                    <button class='btn btn-outline-secondary delParameterNameBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this parameter name'>-</button>\
+                    <button class='btn btn-outline-secondary addParameterBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another parameter'>+</button>\
+                    <button class='btn btn-outline-secondary delParameterBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this parameter'>-</button>\
                 </div>\
             </div>\
         ");
     });
-    
-    $("#funcDeclWrapperRow").on('click', '.delParameterNameBtn', function(event) {
-        console.log("delParameterNameBtn");
-        $("")
+
+    $("#funcDeclWrapperRow").on('click', '.delParameterBtn', function(event) {
+        console.log("delParameterTypeBtn");
+        var eleToBeDeleted = $(this).parents(".repeatableParamListGroup");
+        console.log(eleToBeDeleted);
+        eleToBeDeleted.remove();        
     });
 
+    $("#funcDeclWrapperRow").on('click', '.addFuncDeclBtn', function(event) {
+        console.log("addParameterTypeBtn");
+        $("#funcDeclDivRow").append("\
+            <div class='col-xl-12 col-lg-12 col-md-12 col-12'>\
+                <div class='container-fluid repeatableFuncDeclList'>\
+                    <div class='row dummyToDelete'>\
+                        <div class='col-xl-2 col-lg-2 col-md-2 col-2'>\
+                            <input type='text' class='form-control' placeholder='Return Type' aria-label='Return Type' aria-describedby='basic-addon3'>\
+                        </div>\
+                        <div class='col-xl-2 col-lg-2 col-md-2 col-2'>\
+                            <input type='text' class='form-control' placeholder='Function Name' aria-label='Function Name' aria-describedby='basic-addon4'>\
+                        </div>\
+                        <div class='col-xl-6 col-lg-6 col-md-6 col-6 paramDiv'>\
+                            <div class='input-group repeatableParamListGroup'>\
+                                <input type='text' class='form-control' placeholder='Parameter Type' aria-label='Parameter Type' aria-describedby='basic-addon2'>\
+                                <input type='text' class='form-control' placeholder='Parameter Name' aria-label='Parameter Name' aria-describedby='basic-addon2'>\
+                                <div class='input-group-append'>\
+                                    <button class='btn btn-outline-secondary addParameterBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another parameter'>+</button>\
+                                    <button class='btn btn-outline-secondary delParameterBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this parameter' style='pointer-events: none;' disabled>-</button>\
+                                </div>\
+                            </div>\
+                        </div>\
+                        <div class='col-xl-2 col-lg-2 col-md-2 col-2'>\
+                            <div class='btn-toolbar' role='toolbar' aria-label='Toolbar with button groups'>\
+                                <div class='btn-group mr-2' role='group' aria-label='First group'>\
+                                    <button class='btn btn-secondary addFuncDeclBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another function'>Add</button>\
+                                    <button class='btn btn-danger delFuncDeclBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this function'>Remove</button>\
+                                </div>\
+                            </div>\
+                        </div>\
+                    </div>\
+                </div>\
+            </div>\
+        ");
+    });
+
+    $("#funcDeclWrapperRow").on('click', '.delFuncDeclBtn', function(event) {
+        console.log("delParameterTypeBtn");
+        var eleToBeDeleted = $(this).parents(".dummyToDelete");
+        console.log(eleToBeDeleted);
+        eleToBeDeleted.remove();        
+    });    
+    
 });
