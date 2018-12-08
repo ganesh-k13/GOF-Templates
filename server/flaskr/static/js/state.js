22cab2398723efc05e0bab8dd3384377301d891d
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-12-08 14:13:57 +0530

    Fixed UI bugs

--- server/flaskr/static/js/state.js
+++ server/flaskr/static/js/state.js
@@ -24,4 +24,41 @@ $(document).ready(function() {
         console.log(eleToBeDeleted);
         eleToBeDeleted.remove();
     });
+
+    $("#funcDeclWrapperRow").on('click', '.addParameterTypeBtn', function(event) {
+        console.log("addParameterTypeBtn");
+        $("#paramTypeDiv").append("\
+            <div class='input-group'>\
+                <input type='text' class='form-control' placeholder='Parameter Type' aria-label='Parameter Type' aria-describedby='basic-addon2'>\
+                <div class='input-group-append'>\
+                    <button class='btn btn-outline-secondary addParameterTypeBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another parameter type'>+</button>\
+                    <button class='btn btn-outline-secondary delParameterTypeBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this parameter type'>-</button>\
+                </div>\
+            </div>\
+        ");
+    });
+
+    $("#funcDeclWrapperRow").on('click', '.delParameterTypeBtn', function(event) {
+        console.log("delParameterTypeBtn");
+        $("")
+    });
+    
+    $("#funcDeclWrapperRow").on('click', '.addParameterNameBtn', function(event) {
+        console.log("addParameterNameBtn");
+        $("#paramNameDiv").append("\
+            <div class='input-group'>\
+                <input type='text' class='form-control' placeholder='Parameter Name' aria-label='Parameter Name' aria-describedby='basic-addon2'>\
+                <div class='input-group-append'>\
+                    <button class='btn btn-outline-secondary addParameterNameBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another parameter name'>+</button>\
+                    <button class='btn btn-outline-secondary delParameterNameBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this parameter name'>-</button>\
+                </div>\
+            </div>\
+        ");
+    });
+    
+    $("#funcDeclWrapperRow").on('click', '.delParameterNameBtn', function(event) {
+        console.log("delParameterNameBtn");
+        $("")
+    });
+
 });
