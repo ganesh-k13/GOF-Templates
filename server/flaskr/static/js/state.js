aecb2196cb3fb4bf8886aa9b8595a1c522fa5384
Rahul-RB <rahulbharadwaj033@gmail.com>
2018-12-08 13:48:58 +0530

    Changed UI : Added basic skeleton of future UI

--- server/flaskr/static/js/state.js
+++ server/flaskr/static/js/state.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-	
+    var repeatableGroup = $(".repeatableGroup");
+
+    $("#stateListColDiv").on("click", ".addStateBtn", function(event) {
+        console.log("add clicked",$(this));
+        $("#stateListColDiv").append("\
+            <div class='input-group repeatableGroup'>\
+                <input type='text' class='form-control' placeholder='State Name' aria-label='State Name' aria-describedby='basic-addon2'>\
+                <div class='input-group-append'>\
+                    <button class='btn btn-outline-secondary addStateBtn' type='button' data-toggle='tooltip' data-placement='top' title='Add another state'>\
+                        +\
+                    </button>\
+                    <button class='btn btn-outline-secondary delStateBtn' type='button' data-toggle='tooltip' data-placement='top' title='Delete this state'>\
+                        -\
+                    </button>\
+                </div>\
+            </div>\
+        ");
+    });
+
+    $("#stateListColDiv").on("click", ".delStateBtn", function(event) {
+        console.log("del clicked",$(this));
+        var eleToBeDeleted = $(this).parents(".repeatableGroup");
+        console.log(eleToBeDeleted);
+        eleToBeDeleted.remove();
+    });
 });
