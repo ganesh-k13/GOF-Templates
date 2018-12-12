5e5d9d794b3425ac42bfb48906f0138e265634f9
Ganesh K <ganesh3597@gmail.com>
2018-12-12 11:32:39 +0530

    Frontend for Iterator :collision:

--- server/flaskr/views.py
+++ server/flaskr/views.py
@@ -13,87 +13,91 @@ from GOF_templates import render
 from server.flaskr import utils
 
 tempGlobalVars = {
-    "pattern":"state",   # this name doesn't matter, just a filename for user download
-    "fileType":".tar.gz" # if user forgot to set option then set .tar.gz as default
+	"pattern":"state",	 # this name doesn't matter, just a filename for user download
+	"fileType":".tar.gz" # if user forgot to set option then set .tar.gz as default
 }
 
 app.secret_key = 'secretkeyhereplease'
 
 @app.route("/")
 def home():
-    return render_template("index.html",title="GOF-Templates",isError=False, errorMessage=False)
+	return render_template("index.html",title="GOF-Templates",isError=False, errorMessage=False)
 
 @app.route("/state")
 def state():
-    return render_template("patterns/state.html", title="State", inpValueField1="States", inpValueField2="Functions")
+	return render_template("patterns/state.html", title="State", inpValueField1="States", inpValueField2="Functions")
 
 @app.route("/policy")
 def policy():
-    return render_template("patterns/policy.html", title="Policy", inpValueField1="Policies", inpValueField2="Functions")
+	return render_template("patterns/policy.html", title="Policy", inpValueField1="Policies", inpValueField2="Functions")
 
 @app.route("/singleton")
 def singleton():
-    return render_template("patterns/singleton.html", title="Singleton")
+	return render_template("patterns/singleton.html", title="Singleton")
 
 @app.route("/decorator")
 def decorator():
-    return render_template("patterns/decorator.html", title="Decorator")
+	return render_template("patterns/decorator.html", title="Decorator")
 
 @app.route("/adapter")
 def adapter():
-    return render_template("patterns/adapter.html", title="Adapter")
+	return render_template("patterns/adapter.html", title="Adapter")
 
 @app.route("/mediator")
 def mediator():
-    return render_template("patterns/mediator.html", title="Mediator")
+	return render_template("patterns/mediator.html", title="Mediator")
 
 @app.route("/iterator")
 def iterator():
-    return render_template("patterns/iterator.html", title="Iterator")
+	return render_template("patterns/iterator.html", title="Iterator")
 
 @app.route("/commonCodeCreate",methods=["POST"])
 def commonCodeCreate():
-    payload = request.get_json()
-    
-    if(len(payload["fileType"])<=8): # else, use what user chose
-        tempGlobalVars["fileType"] = payload["fileType"]
-    
-    del payload["fileType"]
+	payload = request.get_json()
+	
+	if(len(payload["fileType"])<=8): # else, use what user chose
+		tempGlobalVars["fileType"] = payload["fileType"]
+	
+	del payload["fileType"]
 
-    print(payload)
+	print(payload)
 
-    if(payload["pattern"] == "state"): # See if there's a way to avoid this via inheritance
-        tempGlobalVars["pattern"] = "state"
-        s = render.State(json.loads(json.dumps(payload)))
-        s.render()
-
-    return jsonify({"success":1})
+	if(payload["pattern"] == "state"): # See if there's a way to avoid this via inheritance
+		tempGlobalVars["pattern"] = "state"
+		s = render.State(json.loads(json.dumps(payload)))
+		s.render()
+	if(payload["pattern"] == "iterator"): # See if there's a way to avoid this via inheritance
+		tempGlobalVars["pattern"] = "iterator"
+		s = render.Iterator(json.loads(json.dumps(payload)))
+		s.render()
+	return jsonify({"success":1})
 
 
 @app.route("/downloadCode",methods=["POST"])
 def downloadCode():
 
-    return redirect(url_for("codeDownload",
-                        filename=tempGlobalVars["pattern"] + tempGlobalVars["fileType"], 
-                        patternType=tempGlobalVars["pattern"], 
-                        fileType=tempGlobalVars["fileType"]))
+	return redirect(url_for("codeDownload",
+						filename=tempGlobalVars["pattern"] + tempGlobalVars["fileType"], 
+						patternType=tempGlobalVars["pattern"], 
+						fileType=tempGlobalVars["fileType"]))
 
 @app.route("/codeDownload/<path:filename>/<path:patternType>/<path:fileType>")
 def codeDownload(filename,patternType,fileType):
-    if(fileType in app.config["ALLOWED_COMPRESSED_FILE_EXTENSIONS"]):
-        beautify_cmd = "astyle --style=attach -s4 -xG -xe -YFpCHUSK --recursive ./GOF_templates/templates/output/" + patternType + "/*.cpp,*.h"
-        os.system(beautify_cmd)
-        os.system("find . -type f -name '*.orig' -exec rm {} +")
-        
-        utils.checkAndCreateDownloadsFolder()
+	print(tempGlobalVars)
+	if(fileType in app.config["ALLOWED_COMPRESSED_FILE_EXTENSIONS"]):
+		beautify_cmd = "astyle --style=attach -s4 -xG -xe -YFpCHUSK --recursive ./GOF_templates/templates/output/" + patternType + "/*.cpp,*.h"
+		os.system(beautify_cmd)
+		os.system("find . -type f -name '*.orig' -exec rm {} +")
+		
+		utils.checkAndCreateDownloadsFolder()
 
-        utils.makeCompressedfile(outputFilename=os.path.join(app.config['CODE_DOWNLOAD_FOLDER'],filename),
-                           sourceDir=os.path.join("./GOF_templates/templates/output/",patternType),
-                           fileType=fileType)
+		utils.makeCompressedfile(outputFilename=os.path.join(app.config['CODE_DOWNLOAD_FOLDER'],filename),
+						   sourceDir=os.path.join("./GOF_templates/templates/output/",patternType),
+						   fileType=fileType)
 
-        print("Path:",os.path.join(app.config["USER_DOWNLOAD_FOLDER"],filename))
+		print("Path:",os.path.join(app.config["USER_DOWNLOAD_FOLDER"],filename))
 
-        return send_from_directory(app.config["USER_DOWNLOAD_FOLDER"],filename,as_attachment=True)
-    else:
-        print("codeDownload error")
-        return (render_template("index.html",isError=True,errorMessage="Please create code by choosing a pattern first."),302)
+		return send_from_directory(app.config["USER_DOWNLOAD_FOLDER"],filename,as_attachment=True)
+	else:
+		print("codeDownload error")
+		return (render_template("index.html",isError=True,errorMessage="Please create code by choosing a pattern first."),302)
