dbacd9500000b812243ff1cadd6595bd422576fa
hardiksurana <hardiksurana01@gmail.com>
2018-12-15 20:44:02 +0530

    removed redundant code

--- GOF_templates/render.py
+++ GOF_templates/render.py
@@ -6,6 +6,30 @@ import os, sys
 class Pattern:
 	def __init__(self, meta):
 		self.meta = meta
+		# Create output
+		self.dirname = os.path.dirname(os.path.abspath(__file__))
+		try:
+			os.makedirs(self.dirname + '/templates/output')
+		except(FileExistsError):
+			# Only folder exists, the file can still be modified
+			# print("\n\nNew Files NOT Created! Delete existing files first! \n\n")
+			pass
+		
+		try: 
+			os.makedirs(self.dirname + '/templates/output/%s'%self.meta['pattern'])
+			os.makedirs(self.dirname + '/templates/output/%s/include'%self.meta['pattern'])
+			os.makedirs(self.dirname + '/templates/output/%s/obj'%self.meta['pattern'])
+			os.makedirs(self.dirname + '/templates/output/%s/src'%self.meta['pattern'])
+			print("\n\nNew Folders Created!\n\n") # New folders created, not files
+		except(FileExistsError):
+			# Only folder exists, the file can still be modified
+			# print("\n\nNew Files NOT Created! Delete existing files first! \n\n")
+			pass
+			
+		# Create meta file
+		with open(self.dirname + '/templates/output/%s/meta.json'%self.meta['pattern'], 'w') as f:
+			print(self.meta)
+			json.dump(self.meta, f, indent=4)
 	
 	def render(self):
 		raise NotImplementedError("Subclass must implement abstract method")
@@ -17,61 +41,47 @@ class State(Pattern):
 		for fn in self.meta['functions']:
 			fn['params'] = list(zip(fn['param_types'], fn['param_names']))
 	
-	def render(self):
-		# Create output
-		dirname = os.path.dirname(os.path.abspath(__file__))
-		try: 
-			os.makedirs(dirname + '/templates/output')
-			os.makedirs(dirname + '/templates/output/state')
-			os.makedirs(dirname + '/templates/output/state/include')
-			os.makedirs(dirname + '/templates/output/state/obj')
-			os.makedirs(dirname + '/templates/output/state/src')
-
-			print("\n\nNew Files Created!\n\n")
-		except(FileExistsError):
-			print("\n\nNew Files NOT Created! Delete existing files first! \n\n")
-			pass
-		
+	def render(self):		
 		# State.h
-		with open(dirname + '/templates/State/include/state.h', 'r') as f:
+		with open(self.dirname + '/templates/State/include/state.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/state/include/state.h', 'w') as f:
+		with open(self.dirname + '/templates/output/state/include/state.h', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], states=self.meta['states']))
 		
 		# State.cpp
-		with open(dirname + '/templates/State/src/state.cpp', 'r') as f:
+		with open(self.dirname + '/templates/State/src/state.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/state/src/state.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/state/src/state.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], states=self.meta['states']))
 		
 		# Context.h
-		with open(dirname + '/templates/State/include/context.h', 'r') as f:
+		with open(self.dirname + '/templates/State/include/context.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/state/include/context.h', 'w') as f:
+		with open(self.dirname + '/templates/output/state/include/context.h', 'w') as f:
 			f.write(template.render(functions=self.meta['functions']))
 		
 		# Context.cpp
-		with open(dirname + '/templates/State/src/context.cpp', 'r') as f:
+		with open(self.dirname + '/templates/State/src/context.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/state/src/context.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/state/src/context.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions']))
 		
 		# makefile
-		with open(dirname + '/templates/State/makefile', 'r') as f:
+		with open(self.dirname + '/templates/State/makefile', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/state/makefile', 'w') as f:
+		with open(self.dirname + '/templates/output/state/makefile', 'w') as f:
 			f.write(template.render())
 		
 		#test.cpp
-		with open(dirname + '/templates/State/test.cpp', 'r') as f:
+		with open(self.dirname + '/templates/State/test.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/state/test.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/state/test.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], states=self.meta['states']))
 
 class Policy(Pattern):
@@ -82,60 +92,46 @@ class Policy(Pattern):
 			fn['params'] = list(zip(fn['param_types'], fn['param_names']))
 	
 	def render(self):
-		# Create output
-		dirname = os.path.dirname(os.path.abspath(__file__))
-		print("\n\nDirname for Policy: {0}\n\n".format(dirname))
-		try: 
-			os.makedirs(dirname + '/templates/output/policy')
-			os.makedirs(dirname + '/templates/output/policy/include')
-			os.makedirs(dirname + '/templates/output/policy/obj')
-			os.makedirs(dirname + '/templates/output/policy/src')
-
-			print("\n\nNew Files Created!\n\n")
-		except(FileExistsError):
-			print("\n\nNew Files NOT Created! Delete existing files first! \n\n")
-			pass
-		
 		# policy.h
-		with open(dirname + '/templates/Policy/include/policy.h', 'r') as f:
+		with open(self.dirname + '/templates/Policy/include/policy.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/policy/include/policy.h', 'w') as f:
+		with open(self.dirname + '/templates/output/policy/include/policy.h', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], policies=self.meta['policies']))
 		
 		# policy.cpp
-		with open(dirname + '/templates/Policy/src/policy.cpp', 'r') as f:
+		with open(self.dirname + '/templates/Policy/src/policy.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/policy/src/policy.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/policy/src/policy.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], policies=self.meta['policies']))
 		
 		# context.h
-		with open(dirname + '/templates/Policy/include/context.h', 'r') as f:
+		with open(self.dirname + '/templates/Policy/include/context.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/policy/include/context.h', 'w') as f:
+		with open(self.dirname + '/templates/output/policy/include/context.h', 'w') as f:
 			f.write(template.render(functions=self.meta['functions']))
 		
 		# context.cpp
-		with open(dirname + '/templates/Policy/src/context.cpp', 'r') as f:
+		with open(self.dirname + '/templates/Policy/src/context.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/policy/src/context.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/policy/src/context.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions']))
 		
 		# makefile
-		with open(dirname + '/templates/Policy/makefile', 'r') as f:
+		with open(self.dirname + '/templates/Policy/makefile', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/policy/makefile', 'w') as f:
+		with open(self.dirname + '/templates/output/policy/makefile', 'w') as f:
 			f.write(template.render())
 		
 		#test.cpp
-		with open(dirname + '/templates/Policy/test.cpp', 'r') as f:
+		with open(self.dirname + '/templates/Policy/test.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/policy/test.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/policy/test.cpp', 'w') as f:
 			f.write(template.render(functions=self.meta['functions'], policies=self.meta['policies']))
 
 class Iterator(Pattern):
@@ -144,58 +140,39 @@ class Iterator(Pattern):
 		Pattern.__init__(self, meta)
 	
 	def render(self):
-		# Create output
-		dirname = os.path.dirname(os.path.abspath(__file__))
-		try:
-			os.makedirs(dirname + '/templates/output')
-		except(FileExistsError):
-			print("\n\nNew Files NOT Created! Delete existing files first! \n\n")
-			pass
-		
-		try: 
-			os.makedirs(dirname + '/templates/output/Iterator')
-			os.makedirs(dirname + '/templates/output/Iterator/include')
-			os.makedirs(dirname + '/templates/output/Iterator/obj')
-			os.makedirs(dirname + '/templates/output/Iterator/src')
-
-			print("\n\nNew Files Created!\n\n")
-		except(FileExistsError):
-			print("\n\nNew Files NOT Created! Delete existing files first! \n\n")
-			pass
-		
 		# container.h
-		with open(dirname + '/templates/Iterator/include/container.h', 'r') as f:
+		with open(self.dirname + '/templates/Iterator/include/container.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/Iterator/include/container.h', 'w') as f:
+		with open(self.dirname + '/templates/output/Iterator/include/container.h', 'w') as f:
 			f.write(template.render(container_name=self.meta['container_name'], iterator_name=self.meta['iterator_name']))
 		
 		# container.cpp
-		with open(dirname + '/templates/Iterator/src/container.cpp', 'r') as f:
+		with open(self.dirname + '/templates/Iterator/src/container.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/Iterator/src/container.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/Iterator/src/container.cpp', 'w') as f:
 			f.write(template.render(container_name=self.meta['container_name'], supported_types=self.meta['supported_types']))
 		
 		# utils.h
-		with open(dirname + '/templates/Iterator/include/utils.h', 'r') as f:
+		with open(self.dirname + '/templates/Iterator/include/utils.h', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/Iterator/include/utils.h', 'w') as f:
+		with open(self.dirname + '/templates/output/Iterator/include/utils.h', 'w') as f:
 			f.write(template.render())
 		
 		# makefile
-		with open(dirname + '/templates/Iterator/makefile', 'r') as f:
+		with open(self.dirname + '/templates/Iterator/makefile', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/Iterator/makefile', 'w') as f:
+		with open(self.dirname + '/templates/output/Iterator/makefile', 'w') as f:
 			f.write(template.render())
 		
 		#test.cpp
-		with open(dirname + '/templates/Iterator/test.cpp', 'r') as f:
+		with open(self.dirname + '/templates/Iterator/test.cpp', 'r') as f:
 			template = Template(f.read())
 		
-		with open(dirname + '/templates/output/Iterator/test.cpp', 'w') as f:
+		with open(self.dirname + '/templates/output/Iterator/test.cpp', 'w') as f:
 			f.write(template.render(container_name=self.meta['container_name']))
 			
 if __name__ == '__main__':
